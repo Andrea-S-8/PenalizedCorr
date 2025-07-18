5b7d20497ced915e749860d3cc8d757eccb26792
Andrea-S-8 <andreastorey29@gmail.com>
2025-07-18 14:23:44 +0100

    updated test for tiny test

--- tests/argtests.R
+++ tests/argtests.R
@@ -145,3 +145,148 @@ expect_error(ar.penyw(y2), "'x' must be numeric")
 
 
 
+
+
+set.seed(12345)
+x1 = arima.sim(model=list(ar=0.4),n=500)
+x2 = arima.sim(model=list(ar=0.8),n=500)
+x=cbind(x1, x2)
+
+#testing that PenalizedCorr can only be logical
+### acf ###
+expect_error(acf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(acf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(acf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(acf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### pacf ###
+expect_error(pacf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(pacf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(pacf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(pacf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### ar ###
+########
+#ar doesn't find penalized so all failed even when should have passed
+########
+expect_error(ar(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(ar(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### auto.acf ###
+########
+#auto.acf doesn't find penalized so first is failed when should have passed
+########
+expect_error(auto.acf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(auto.acf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(auto.acf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(auto.acf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### invertpacf ###
+########
+#invertpacf doesn't find penalized so first is failed when should have passed
+########
+expect_error(invertpacf(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(invertpacf(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(invertpacf(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(invertpacf(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### DLpencoaf ###
+expect_error(DLpencoef(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(DLpencoef(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(DLpencoef(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(DLpencoef(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+### ar.penyw ###
+########
+#ar doesn't find penalized so all failed even when should have passed
+########
+expect_error(ar.penyw(x,penalized =TRUE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar.penyw(x,penalized =FALSE), "penalized must be logical")
+#Failed as no errors found as expected
+expect_error(ar.penyw(x,penalized =NULL), "penalized must be logical")
+#Passed as errors found as expected
+expect_error(ar.penyw(x,penalized = 3), "penalized must be logical")
+#Passed as errors found as expected
+
+
+
+
+##testing that data series can only be numeric
+#### acf ####
+################
+#fails but not for the right reasons
+################
+expect_error(acf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### pacf ####
+expect_error(pacf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### auto.acf ####
+################
+#fails but not for the right reasons
+################
+expect_error(auto.acf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### invertpacf ####
+################
+#fails but not for the right reasons
+################
+expect_error(invertpacf(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### DLpencoef ####
+expect_error(DLpencoef(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### ar ####
+################
+#fails but not for the right reasons
+################
+expect_error(ar(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+#### ar.penyw ####
+################
+#fails but not for the right reasons
+################
+expect_error(ar.penyw(x), "'x' must be numeric")
+#Failed as no error found as expected
+
+
+
+
+set.seed(12345)
+m <- arima.sim(n=100, model=list(ar=0.8))
+#expect_equal(m, readRDS("./tests/expected/equals-error/m"))
+
+
